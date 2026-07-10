91b593fefbec3a526b22406dd8836cd92c78f43e
Prabin Giri <prabin17np@gmail.com>
2026-07-11 03:15:58 +0545

    feat: adding routes

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,49 @@
-const express = require("express");
-const helmet = require("helmet");
-require("dotenv").config();
+const express = require('express');
+const helmet = require('helmet');
+const session = require('express-session');
+require('dotenv').config();
 
-require("./config/database");
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
+// Security headers
 app.use(helmet());
+
+// Parse JSON requests
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("SecureDMS Backend Running");
+// Session configuration
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false, // change to true when using HTTPS
+      maxAge: 1000 * 60 * 30 // 30 minutes
+    }
+  })
+);
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Health check
+app.get('/', (req, res) => {
+  res.json({
+    message: 'SecureDMS API running'
+  });
+});
+
+// Error handler
+app.use((err, req, res, next) => {
+  console.error(err.message);
+
+  res.status(500).json({
+    message: 'Internal server error'
+  });
 });
 
 module.exports = app;
