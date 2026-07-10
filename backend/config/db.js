4fda81ae8d8904cd778c020a415258a736a80c24
Prabin Giri <prabin17np@gmail.com>
2026-07-11 03:11:49 +0545

    fix: database name

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const pool = new Pool({
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME || 'sdms',
+  database: process.env.DB_NAME,
   max: 10,                     // max connections in the pool
   idleTimeoutMillis: 30000,
 });
